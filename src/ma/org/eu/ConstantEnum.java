ce8607074233eb79e91c106187a592d2b5102319
ezscript <ezscript@126.com>
2015-06-18 09:15:22 +0800

    enum

---
+++ src/ma/org/eu/ConstantEnum.java
@@ -0,0 +1,29 @@
+package ma.org.eu;
+
+public enum ConstantEnum {
+	A,
+	B,
+	C,
+	D,
+	E;
+	
+	public static void main(String [] args){
+		read(ConstantEnum.A);
+		
+	}
+	
+	public static void read(ConstantEnum ce){
+		switch(ce){
+			case A:
+				break;
+			case B:
+				break;
+			case C:
+				break;
+			case D:
+				break;
+			case E:
+				break;
+		}
+	}
+}
---
+++ src/ma/org/eu/SingleEnum.java
@@ -0,0 +1,12 @@
+package ma.org.eu;
+
+public enum SingleEnum {
+	INSTANCE;
+	private String a;
+	private Object b;
+	
+	public static void init(){
+		
+	}
+	
+}	
